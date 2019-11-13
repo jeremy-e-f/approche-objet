5aff459a42a4e9cbfcc5182427ff438a33f18a92
Jeremy FROMINVILLE <j.e.fromin@gmail.com>
2019-11-13 14:39:18 +0100

    TP3

--- src/main/java/entites/Personne.java
+++ src/main/java/entites/Personne.java
@@ -10,6 +10,11 @@ public class Personne {
 		
 	}
 	
+	public Personne(String nom, String prenom){
+		this.nom= nom;
+		this.prenom= prenom;
+	}
+	
 	public Personne(String nom, String prenom, AdressePostale adresse){
 		this.nom= nom;
 		this.prenom= prenom;
