d4cbe77e66d579b940aa0fc79ef8cded18cf7d7a
Jeremy FROMINVILLE <j.e.fromin@gmail.com>
2019-11-13 16:04:17 +0100

    TP4

---
+++ src/main/java/utils/ConversionNombre.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class ConversionNombre {
+	
+	public static void main(String[] args){
+		String nombre= "42";
+		int number= Integer.parseInt(nombre);
+		
+		int a= 58, b=626;
+		int resultat= Integer.max(a,b);
+		System.out.println(resultat);
+	}
+}
